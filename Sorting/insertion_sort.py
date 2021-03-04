d5d7952ede07fdd11b7ec53cb5039060ad0812b8
himel999 <mahabubulalamofficial@gmail.com>
2021-03-04 23:22:33 +0600

    insertion sort

---
+++ Sorting/insertion_sort.py
@@ -0,0 +1,21 @@
+def insertion_sort(lst):
+
+    for i in range(1, len(lst)):
+        anchor = lst[i]
+
+        j = i - 1
+
+        while j >= 0 and anchor < lst[j]:
+            print(j+1, j)
+            lst[j+1] = lst[j]
+            j = j - 1
+            print(j)
+
+        lst[j+1] = anchor
+
+
+if __name__ == "__main__":
+
+    elements = [21, 38, 29, 17, 4, 25, 11, 32, 9]
+    insertion_sort(elements)
+    print(elements)
