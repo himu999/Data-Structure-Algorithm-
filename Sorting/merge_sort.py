dd45c1e8d394f89d7f1e2db998429b3c9c3a835f
himel999 <mahabubulalamofficial@gmail.com>
2021-03-06 22:46:41 +0600

    merge sort

--- Sorting/merge_sort.py
+++ Sorting/merge_sort.py
@@ -1,34 +1,43 @@
-def merge_two_sorted_array(a, b):
+def merge_sort(lst):
+    mid = len(lst) // 2
 
+    if len(lst) <= 1:
+        return lst
+
+    left = lst[:mid]
+    right = lst[mid:]
+
+    left = merge_sort(left)
+    right = merge_sort(right)
+
+    return merge_two_list(left, right)
+
+
+def merge_two_list(arr1, arr2):
     c = []
-
-    len_a = len(a)
-    len_b = len(b)
-
+    len_a = len(arr1)
+    len_b = len(arr2)
     i = j = 0
     while i < len_a and j < len_b:
-
-        if a[i] <= b[j]:
-            c.append(a[i])
+        if arr1[i] < arr2[j]:
+            c.append(arr1[i])
             i = i + 1
         else:
-            c.append(b[j])
+            c.append(arr2[j])
             j = j + 1
 
     while i < len_a:
-        c.append(a[i])
+        c.append(arr1[i])
+
         i = i + 1
 
     while j < len_b:
-        c.append(a[j])
+        c.append(arr2[j])
         j = j + 1
 
     return c
 
 
 if __name__ == "__main__":
-
-    a = [5, 8, 12, 56]
-    b = [7, 9, 45, 51]
-
-    print(merge_two_sorted_array(a, b))
+    z = [10, 3, 15, 7, 8, 23, 98, 29]
+    print(merge_sort(z))
