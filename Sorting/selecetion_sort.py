f63fc7f8d0ed9fd4d97cba2bfe65089b7cbde1cf
himel999 <mahabubulalamofficial@gmail.com>
2021-03-07 14:00:05 +0600

    selection sort with different example

--- Sorting/selecetion_sort.py
+++ Sorting/selecetion_sort.py
@@ -1,17 +1,34 @@
 def merge_sort(lst):
 
-    for i in range(len(lst)):
+    for i in range(len(lst) - 1):
         min_index = i
 
         for j in range(min_index + 1, len(lst)):
-            if lst[min_index] > lst[j]:
+            if str(lst[min_index]) > str(lst[j]):
                 min_index = j
 
-        lst[min_index], lst[i] = lst[i], lst[min_index]
+        if i != min_index:
+            lst[min_index], lst[i] = lst[i], lst[min_index]
 
     return lst
 
 
 if __name__ == "__main__":
-    a = [10, 5, -9, 45, 0, 753, -963]
+    a = [
+        {'First Name': 'Raj', 'Last Name': 'Nayyar'},
+        {'First Name': 'Suraj', 'Last Name': 'Sharma'},
+        {'First Name': 'Karan', 'Last Name': 'Kumar'},
+        {'First Name': 'Jade', 'Last Name': 'Canary'},
+        {'First Name': 'Raj', 'Last Name': 'Thakur'},
+        {'First Name': 'Raj', 'Last Name': 'Sharma'},
+        {'First Name': 'Kiran', 'Last Name': 'Kamla'},
+        {'First Name': 'Armaan', 'Last Name': 'Kumar'},
+        {'First Name': 'Jaya', 'Last Name': 'Sharma'},
+        {'First Name': 'Ingrid', 'Last Name': 'Galore'},
+        {'First Name': 'Jaya', 'Last Name': 'Seth'},
+        {'First Name': 'Armaan', 'Last Name': 'Dadra'},
+        {'First Name': 'Ingrid', 'Last Name': 'Maverick'},
+        {'First Name': 'Aahana', 'Last Name': 'Arora'}
+    ]
     print(merge_sort(a))
+
