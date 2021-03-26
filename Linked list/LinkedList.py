0f8f2641d90a61160c196ba75c4ef86c9c539096
himu999 <mahabubulalamofficial@gmail.com>
2021-03-26 23:24:45 +0600

    insert data at the end

--- Linked list/LinkedList.py
+++ Linked list/LinkedList.py
@@ -8,10 +8,9 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
-    def insert_at_begining(self, data):
+    def insert_at_beginning(self, data):
         node = Node(data, self.head)
         self.head = node
-        print('Before node', node)
 
     def print(self):
         if self.head is None:
@@ -27,15 +26,25 @@ class LinkedList:
 
         print(llstr)
 
+    def insert_at_end(self, data):
+        if self.head is None:
+            self.head = Node(data, None)
+            return
+
+        itr = self.head
+
+        while itr.next:
+            itr = itr.next
+
+        itr.next = Node(data, None)
+
 
 if __name__ == "__main__":
     ll = LinkedList()
 
-    ll.insert_at_begining(98)
-    ll.insert_at_begining(98)
-    ll.insert_at_begining(98)
-    ll.insert_at_begining(98)
-    ll.insert_at_begining(98)
-    ll.insert_at_begining(89)
-    ll.print()
+    ll.insert_at_beginning(98)
+    ll.insert_at_beginning(908)
+    ll.insert_at_beginning(980)
+    ll.insert_at_end(79)
 
+    ll.print()
