5674a1af290af391fbf2b9ae8ec5bba09f9a06bd
himu999 <mahabubulalamofficial@gmail.com>
2021-03-27 23:25:38 +0600

    insert data at the beginning insert data at the end insert values length of linked list

--- Linked list/LinkedList.py
+++ Linked list/LinkedList.py
@@ -8,23 +8,9 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
-    def insert_at_beginning(self, data):
+    def insert_at_beginning(self, data=None):
         node = Node(data, self.head)
         self.head = node
-
-    def print(self):
-        if self.head is None:
-            print("Linked list is empty")
-            return
-
-        itr = self.head
-        llstr = ''
-
-        while itr:
-            llstr += str(itr.data) + '-->'
-            itr = itr.next
-
-        print(llstr)
 
     def insert_at_end(self, data):
         if self.head is None:
@@ -32,19 +18,56 @@ class LinkedList:
             return
 
         itr = self.head
-
         while itr.next:
             itr = itr.next
-
         itr.next = Node(data, None)
+
+    def insert_values(self, data_list):
+        for data in data_list:
+            self.insert_at_end(data)
+
+    def length_of_ll(self):
+        itr = self.head
+        cont = 0
+
+        while itr:
+            cont += 1
+            itr = itr.next
+
+        return print(cont)
+
+
+    def print(self):
+
+        if self.head is None:
+            print("Empty Linked List")
+            return
+
+        itr = self.head
+        llsr = ""
+
+        while itr:
+            llsr += str(itr.data)+"-->"
+            itr = itr.next
+
+        print(llsr)
 
 
 if __name__ == "__main__":
-    ll = LinkedList()
 
-    ll.insert_at_beginning(98)
-    ll.insert_at_beginning(908)
-    ll.insert_at_beginning(980)
-    ll.insert_at_end(79)
-
-    ll.print()
+    llst = LinkedList()
+    dat_list = ["mango", "banana", "guava"]
+    llst.insert_values(dat_list)
+    llst.insert_at_beginning(10)
+    llst.insert_at_beginning(15)
+    llst.insert_at_end(5)
+    llst.insert_at_end(15)
+    llst.insert_at_end(25)
+    llst.insert_at_end(35)
+    llst.insert_at_end(45)
+    llst.insert_at_end(55)
+    llst.insert_at_end(65)
+    llst.insert_at_end(75)
+    llst.insert_at_end(85)
+    llst.print()
+    llst.length_of_ll()
