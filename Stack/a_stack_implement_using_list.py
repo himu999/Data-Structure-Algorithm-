d4728aa44cbb4a05c6db7359bcd595266023f6cb
himu999 <mahabubulalamofficial@gmail.com>
2021-05-11 14:05:09 +0600

    stack implement using list

---
+++ Stack/a_stack_implement_using_list.py
@@ -0,0 +1,30 @@
+stack = []
+
+
+def push():
+    element = input('Enter the element: ')
+    stack.append(element)
+    print(stack)
+
+
+def pop():
+    if not stack:
+        print('Stack is empty!')
+    else:
+        e = stack.pop()
+        print('Remove element', e)
+        print(stack)
+
+
+while True:
+    print("Select the operation 1.push 2.pop 3.quit")
+    choice = int(input())
+
+    if choice == 1:
+        push()
+    elif choice == 2:
+        pop()
+    elif choice == 3:
+        break
+    else:
+        print('Enter the correct operation')
